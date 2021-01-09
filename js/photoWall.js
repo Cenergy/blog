eca5288eddc241d0b878a3252f6211c3aa0f9ba5
cenergy <673598118@qq.com>
2021-01-09 22:11:53 +0800

    Site updated: 2021-01-09 22:11:48

--- js/photoWall.js
+++ js/photoWall.js
@@ -1,5 +1,5 @@
-var imgDataPath = "/blog/photos/photos.json"; //图片名称高宽信息json文件路径
-var imgPath = "https://cdn.jsdelivr.net/gh/Cenergy/images/gallery/"; //图片访问路径
+var imgDataPath = "/blog/photos/photoslist.json"; //图片名称高宽信息json文件路径
+var imgPath = "/blog/photos/images/"; //图片访问路径
 var imgMaxNum = 50; //图片显示数量
 
 var windowWidth =
@@ -12,7 +12,7 @@ if (windowWidth < 768) {
   var imageWidth = 250; //图片显示宽度
 }
 
-const photo = {
+photo = {
   page: 1,
   offset: imgMaxNum,
   init: function () {
@@ -20,37 +20,12 @@ const photo = {
     $.getJSON(imgDataPath, function (data) {
       that.render(that.page, data);
       //that.scroll(data);
-      that.eventListen(data);
     });
   },
-  constructHtml(options) {
-    const {
-      imageWidth,
-      imageX,
-      imageY,
-      name,
-      imgPath,
-      imgName,
-      imgNameWithPattern,
-    } = options;
-    const htmlEle = `<div class="card lozad" style="width:${imageWidth}px">
-                  <div class="ImageInCard" style="height:${
-                    (imageWidth * imageY) / imageX
-                  }px">
-                    <a data-fancybox="gallery" href="${imgPath}${name}/${imgNameWithPattern}"
-                          data-caption="${imgName}" title="${imgName}">
-                            <img  class="lazyload" data-src="${imgPath}${name}/${imgNameWithPattern}"
-                            src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-                            onload="lzld(this)"
-                            lazyload="auto">
-                        </a>
-                  </div>
-                </div>`;
-    return htmlEle;
-  },
-  render: function (page, data = []) {
-    this.data = data;
-    if (!data.length) return;
+  render: function (page, data=[]) {
+    var begin = (page - 1) * this.offset;
+    var end = page * this.offset;
+    if (begin >= data.length) return;
     var html,
       imgNameWithPattern,
       imgName,
@@ -58,79 +33,24 @@ const photo = {
       imageX,
       imageY,
       li = "";
-
-    let liHtml = "";
-    let contentHtml = "";
-
-    data.forEach((item, index) => {
-      const activeClass = index === 0 ? "active" : "";
-      liHtml += `<li class="nav-item" role="presentation">
-          <a class="nav-link ${activeClass} photo-tab" id="home-tab" photo-uuid="${item.name}" data-toggle="tab" href="#${item.name}"  role="tab" aria-controls="${item.name}" aria-selected="true">${item.name}</a>
-        </li>`;
-    });
-    const [initData = {}] = data;
-    const { children = [],name } = initData;
-    children.forEach((item, index) => {
-      imgNameWithPattern = item.split(" ")[1];
+    for (var i = begin; i < end && i < data.length; i++) {
+      imgNameWithPattern = data[i].split(" ")[1];
       imgName = imgNameWithPattern.split(".")[0];
-      imageSize = item.split(" ")[0];
+      imageSize = data[i].split(" ")[0];
       imageX = imageSize.split(".")[0];
       imageY = imageSize.split(".")[1];
-      let imgOptions = {
-        imageWidth,
-        imageX,
-        imageY,
-        name,
-        imgName,
-        imgPath,
-        imgNameWithPattern,
-      };
-      li += this.constructHtml(imgOptions);
-    });
-    contentHtml += ` <div class="tab-pane fade show active"  role="tabpanel" aria-labelledby="home-tab">${li}</div>`;
-
-    const ulHtml = `<ul class="nav nav-pills mb-3 photo-tabs" id="pills-tab" role="tablist">${liHtml}</ul><div id="helloTest"></div>`;
-    const tabContent = `<div class="tab-content" id="myTabContent">${contentHtml}</div>`;
-
-    $("#imageTab").append(ulHtml);
-    $(".ImageGrid").append(tabContent);
+      li +=`<div class="card lozad" style="width:${imageWidth}px">
+                <div class="ImageInCard" style="height:${(imageWidth * imageY) / imageX}px">
+                  <a data-fancybox="gallery" href="${imgPath}${imgNameWithPattern}"
+                    data-caption="${imgName}" title="${imgName}">
+                      <img  class="lazyload" data-src="${imgPath}${imgNameWithPattern}"
+                      src="${imgPath}${imgNameWithPattern}">
+                  </a>
+                </div>
+            </div>`;
+    }
+    $(".ImageGrid").append(li);
     this.minigrid();
-  },
-  eventListen: function (data) {
-    let self = this;
-    var html,
-      imgNameWithPattern,
-      imgName,
-      imageSize,
-      imageX,
-      imageY,
-      li = "";
-    $('a[data-toggle="tab"]').on("shown.bs.tab", function (e) {
-      $(".ImageGrid").empty();
-      const selectId = $(e.target).attr("photo-uuid");
-      const selectedData = data.find((data) => data.name === selectId) || {};
-      const { children,name } = selectedData;
-      let li = "";
-      children.forEach((item, index) => {
-        imgNameWithPattern = item.split(" ")[1];
-        imgName = imgNameWithPattern.split(".")[0];
-        imageSize = item.split(" ")[0];
-        imageX = imageSize.split(".")[0];
-        imageY = imageSize.split(".")[1];
-        let imgOptions = {
-          imageWidth,
-          imageX,
-          imageY,
-          name,
-          imgName,
-          imgPath,
-          imgNameWithPattern,
-        };
-        li += self.constructHtml(imgOptions);
-      });
-      $(".ImageGrid").append(li);
-      self.minigrid();
-    });
   },
   minigrid: function () {
     var grid = new Minigrid({
