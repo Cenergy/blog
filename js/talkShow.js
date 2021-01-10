4b540a87fd2ebb451d28e3e4c491b7d2dd8b7ba3
cenergy <673598118@qq.com>
2021-01-10 15:49:59 +0800

    Site updated: 2021-01-10 15:49:08

---
+++ js/talkShow.js
@@ -0,0 +1,8 @@
+new Artitalk({
+    appId: 'diTi74ah5rpPDNa6bjS0VjqN-gzGzoHsz', // Your LeanCloud appId
+    appKey: 'nHbjOKICQKYQtsVVJjeHYl0O', // Your LeanCloud appKey
+    pageSize: 6,
+    shuoPla: "小声哔哔..."
+})
+
+
