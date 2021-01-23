c63f3cdd3336c732e365429097b957061192af98
cenergy <673598118@qq.com>
2021-01-24 00:02:56 +0800

    Site updated: 2021-01-24 00:02:55

--- js/comment2.js
+++ js/comment2.js
@@ -2,6 +2,6 @@ console.log("rdapp - window", window)
 new Waline({
   el: "#waline",
   path: location.pathname,
-  serverURL: "https://waline.aigisss.com/",
+  serverURL: "https://wvline.vercel.app/",
   placeholder: "留下您的意见或建议...(审核后可见)!",
 });
